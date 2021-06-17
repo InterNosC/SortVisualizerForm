78a4a037abe4dca74a4b68ce0d67a6b3d6f722d1
InterNos <kirilrknu@gmail.com>
2021-06-17 10:37:33 +0300

    Add documantation

---
+++ SortVisualizerForm/html/search/all_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['dispose_6',['Dispose',['../class_sort_visualizer_form_1_1_sort_desktop.html#a111569330b5497004e140dbe9c7fa985',1,'SortVisualizerForm::SortDesktop']]]
+];
---
+++ SortVisualizerForm/html/search/functions_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['bgw_5fdowork_36',['BGW_DoWork',['../class_sort_visualizer_form_1_1_sort_desktop.html#a351088d06ca1e7203194cebff0fd6a76',1,'SortVisualizerForm::SortDesktop']]],
+  ['bubblesort_37',['BubbleSort',['../class_sort_visualizer_form_1_1_algoritms_1_1_bubble_sort.html#a2939cd139e93f09307a595d308290907',1,'SortVisualizerForm::Algoritms::BubbleSort']]],
+  ['bubblesorting_38',['BubbleSorting',['../class_sort_visualizer_form_1_1_algoritms_1_1_bubble_sort.html#ad3d08c1aae0a6196f7e59215f2afb571',1,'SortVisualizerForm::Algoritms::BubbleSort']]]
+];
